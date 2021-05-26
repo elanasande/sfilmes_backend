51bc9c8b78d21b86763c1af56ac14ea926aef43a
Elana <elanatanan@gmail.com>
2021-05-26 18:48:50 -0300

    Inicio e criacao do modeule usuario

---
+++ modules/Usuario.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Usuario = new mongoose.Schema({
+  email: {
+    type: String,
+    default: ''
+  },
+  password: {
+    type: String,
+    default: ''
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false
+  },
+  signUpDate: {
+    type: Date,
+    default: Date.now()
+  }
+});
+UsuarioSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+UsuarioSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+module.exports = mongoose.model('Usuario', UsuarioSchema);
